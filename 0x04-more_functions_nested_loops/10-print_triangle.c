c08cd2ff8a6d5c9554fe6f7183bc54d3207b4fef
mikewedam <michael.abocco@gmail.com>
2023-07-21 07:16:41 -0700

    10-main.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_triangle - prints a triangle, followed by a new line
- * @size: size of the triangle
+ * print_triangel - prints a triangel, followed by a new line
+ * @size: size of the triangel
  */
-void print_triangle(int size)
+void print_triangel(int size)
 {
 	if (size <= 0)
 	{
@@ -28,23 +28,4 @@ void print_triangle(int size)
 			_putchar('\n');
 		}
 	}
-}
-
-
-10-main.c
-
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_triangle(2);
-    print_triangle(10);
-    print_triangle(1);
-    print_triangle(0);
-    return (0);
 }
