1b704858c15f2ceac14e4fba3d1d178f85449685
mikewedam <michael.abocco@gmail.com>
2023-08-30 05:40:24 -0700

    update

--- 0x13-more_singly_linked_lists/103-main.c
+++ 0x13-more_singly_linked_lists/103-main.c
@@ -12,18 +12,21 @@ int main(void)
 {
 	listint_t *head;
 	listint_t *head2;
+	listint_t *node;
 
 	head2 = NULL;
 	add_nodeint(&head2, 0);
-	add_nodeint(&head2, 0);
-	add_nodeint(&head2, 0);
-	add_nodeint(&head2, 0);
-	add_nodeint(&head2, 0);
-	add_nodeint(&head2, 0);
-	add_nodeint(&head2, 0);
-	add_nodeint(&head2, 0);
+	add_nodeint(&head2, 1);
+	add_nodeint(&head2, 2);
+	add_nodeint(&head2, 3);
+	add_nodeint(&head2, 4);
+	add_nodeint(&head2, 98);
+	add_nodeint(&head2, 402);
+	add_nodeint(&head2, 1024);
 	print_listint_safe(head2);
+
 	node = find_listint_loop(head2);
+
 	if (node != NULL)
 	{
 		printf("Loop starts at [%p] %d\n", (void *)node, node->n);
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - Adds a new node at the end of a linked list.
- * @head: Pointer to the first element in the list.
- * @n: Data to insert in the new element.
+ * add_nodeint_end - adds a node at the end of a linked list
+ * @head: pointer to the pointer to the first element in the list
+ * @n: data to insert in the new element
  *
- * Return: Pointer to the new node, or NULL if it fails.
+ * Return: pointer to the new node, or NULL if it fails
  */
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
-
 	if (!new_node)
+	{
 		return (NULL);
+	}
 
 	new_node->n = n;
 	new_node->next = NULL;
@@ -21,16 +21,16 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return (new_node);
 	}
-
-	listint_t *temp = *head;
-	while (temp->next != NULL)
+	else
 	{
-		temp = temp->next;
+		listint_t *temp = *head;
+		while (temp->next)
+		{
+			temp = temp->next;
+		}
+		temp->next = new_node;
 	}
-
-	temp->next = new_node;
 
 	return (new_node);
 }
