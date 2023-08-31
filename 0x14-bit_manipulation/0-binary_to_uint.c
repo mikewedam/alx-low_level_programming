5c4c1deb9eb7442aecf01e22d0b3441019b330b0
mikewedam <michael.abocco@gmail.com>
2023-08-31 07:49:05 -0700

    update

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * convert_binary_to_decimal - Converts a binary string to its decimal equivalent.
+ * @binary_str: The input binary string.
+ *
+ * Return: The decimal equivalent of the binary string.
+ */
+unsigned int convert_binary_to_decimal(const char *binary_str)
+{
+	int index;
+	unsigned int decimal_value = 0;
+
+	if (!binary_str)
+		return (0);
+
+	for (index = 0; binary_str[index]; index++)
+	{
+		if (binary_str[index] < '0' || binary_str[index] > '1')
+			return (0);
+		decimal_value = 2 * decimal_value + (binary_str[index] - '0');
+	}
+
+	return (decimal_value);
+}
